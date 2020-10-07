1f2a1a5433cb309f7afe52bb65699e467252feab
Aljawharah Aldukhayni <aljawharah.aldukhayni@gmail.com>
2020-10-07 15:48:59 +0300

    Update __init__.py
    Adding win conditions

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
             '4': ' ' , '5': ' ' , '6': ' ' ,
             '1': ' ' , '2': ' ' , '3': ' ' }
+
+
             
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
@@ -9,41 +11,58 @@ def printBoard(board):
     print('-+-+-')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
     
- 
 
+                    
 Role='X'
 count=0
 
-for i in range(10):
+for i in range(9):
+      
+                
     printBoard(theBoard)
     inp=input(Role + " Role, choose a place for "+ Role + "\n")
     
+
+            
     if theBoard[inp]==' ':
         theBoard[inp] = Role
-        count=+1
+        count=count+1
+        
     
     else:
         print('This place is already exists')
         continue
-        
+                                   
 # Now we will check if player X or O has won,for every move after 5 moves. 
+ 
+    if count>=5:
     
-    if count >= 5:
-        
         if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")    
-                break
-                
+            printBoard(theBoard)
+            print("\nGame Over.\n")                
+            print(" **** " +Role + " won. ****")    
+            break
+
+
         elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ':
-                printBoard(theBoard)
-                print("\nGame Over.\n")                
-                print(" **** " +turn + " won. ****")
-                break
-                        
-        
+            printBoard(theBoard)
+            print("\nGame Over.\n")                
+            print(" **** " +Role + " won. ****")
+            break
+            
+            
+        elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ':
+            printBoard(theBoard)
+            print("\nGame Over.\n")                
+            print(" **** " +Role + " won. ****")
+            break
+            
     if Role =='X':
        Role = 'O'
     else:
        Role = 'X'     
+
+    
+
+   
+
